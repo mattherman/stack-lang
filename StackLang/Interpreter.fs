72e2cd30e3e378f29d3a2e2b152b8bf076184a4c
Matthew Herman <mattherman11@gmail.com>
2022-12-24 09:56:43 -0600

    Initial commit

---
+++ StackLang/Interpreter.fs
@@ -0,0 +1,90 @@
+module Interpreter
+
+open System
+
+type Interpreter = {
+    Vocabulary: Map<string, Word>;
+    Stack: Value list
+    Compiling: bool;
+}
+and Value =
+    | Literal of int
+    | Word of Word
+and Instructions =
+    | Native of (Value list -> Result<Value list, string>)
+    | Compiled of string[]
+and Word = {
+    Symbol: string
+    Instructions: Instructions
+}
+
+let split count list =
+    (List.take count list, List.skip count list)
+
+let getParameters count (stack: Value list) =
+    if stack.Length >= count then
+        Ok (stack |> split count)
+    else
+        Error "Stack underflow"
+        
+let NativeAdd (stack: Value list) =
+    stack
+    |> getParameters 2
+    |> Result.bind (fun (parameters, remainingStack) ->
+        match (parameters[0], parameters[1]) with
+        | Literal first, Literal second ->
+            let newValue = Literal (second + first)
+            Ok (newValue::remainingStack)
+        | _ ->
+            Error "Values do not support (+)")
+
+let NativeSubtract (stack: Value list) =
+    stack
+    |> getParameters 2
+    |> Result.bind (fun (parameters, stack) ->
+        match (parameters[0], parameters[1]) with
+        | Literal first, Literal second ->
+            let newValue = Literal (second - first)
+            Ok (newValue::stack)
+        | _ ->
+            Error "Values do not support (-)")
+    
+let createInterpreter () =
+    let nativeWords = [
+        ("+", { Symbol = "+"; Instructions = Native NativeAdd })
+        ("-", { Symbol = "-"; Instructions = Native NativeSubtract })
+    ]
+    let vocab = nativeWords |> Map.ofList
+    { Vocabulary = vocab; Stack = []; Compiling = false }
+    
+let executeWord interpreter word =
+    printfn $"Executing word: %s{word.Symbol}"
+    match word.Instructions with
+    | Native nativeWord ->
+        nativeWord interpreter.Stack
+        |> Result.map (fun newStack -> { interpreter with Stack = newStack })
+    | Compiled compiledWord ->
+        printfn $" => %A{compiledWord}"
+        Ok interpreter
+    
+let executeLiteral interpreter (token: string) =
+    match Int32.TryParse(token) with
+    | true, number ->
+        let literal = Literal number
+        Ok { interpreter with Stack = literal::interpreter.Stack }
+    | _ ->
+        Error $"Unable to parse literal: {token}"
+
+let next interpreter token =
+    match interpreter.Vocabulary.TryGetValue(token) with
+    | true, word -> executeWord interpreter word
+    | _ -> executeLiteral interpreter token
+    
+let run interpreter (input: string) =
+    input.Split " "
+    |> Array.toList
+    |> List.fold (fun lastResult nextToken ->
+        match lastResult with
+        | Error msg -> Error msg
+        | Ok interpreter ->
+            next interpreter nextToken) (Ok interpreter)
---
+++ StackLang/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open System.IO
+open Interpreter
+
+let runFile file =
+    let lines = File.ReadAllLines(file)
+    let input = String.concat " " lines
+    run (createInterpreter ()) input |> ignore
+
+let runRepl () =
+    let mutable quit = false
+    let mutable interpreter = createInterpreter ()
+    while not quit do
+        printf "> "
+        let input = Console.ReadLine()
+        if input = "#quit" then
+            quit <- true
+        else
+            let result = run interpreter input
+            match result with
+            | Ok nextState ->
+                interpreter <- nextState
+            | Error msg ->
+                printfn $"Error: %s{msg}"
+            interpreter.Stack
+            |> List.iter (fun value ->
+                match value with
+                | Literal l -> printfn $"%d{l}"
+                | Word w -> printfn $"%s{w.Symbol}")
+
+let args = Environment.GetCommandLineArgs()
+match args with
+| [|_; file|] -> runFile file
+| _ -> runRepl ()
