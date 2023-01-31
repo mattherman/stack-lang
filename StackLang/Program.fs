8b67c20deb30ad0e907f26aaa9f2b8d1c47f296c
Matthew Herman <mattherman11@gmail.com>
2023-01-31 16:48:31 -0600

    Improved REPL output

--- StackLang/Program.fs
+++ StackLang/Program.fs
@@ -16,20 +16,39 @@ let runRepl () =
     let mutable quit = false
     let mutable interpreter = Interpreter.createInterpreter ()
     attach interpreter
+
+    printfn "Interpreter started. Enter #help for more information."
     while not quit do
-        printf "> "
+        printf "\n> "
         let input = Console.ReadLine()
         match input with
+        | "#help" ->
+            printfn "Enter input to execute or a command."
+            printfn "\nThe following commands are available:"
+            printfn "#quit - exits the interpreter"
+            printfn "#debug - attaches a debugger (attached by default)"
+            printfn "#nodebug - detaches the debuggger"
+            printfn "#step - attaches a debugger and enables step debugging"
+            printfn "#nostep - disables step debugging"
+            printfn "#help - displays this help information"
+            printfn "\nhttps://github.com/mattherman/stack-lang"
         | "#quit" ->
             quit <- true
+            printfn "Goodbye!"
         | "#debug" ->
             attach interpreter
+            printfn "Debugger attached."
         | "#nodebug" ->
             detach interpreter
+            printfn "Debugger detached."
         | "#step" ->
             setStep interpreter true
+            printfn "Step debugging enabled."
         | "#nostep" ->
             setStep interpreter false
+            printfn "Step debugging disabled."
+        | input when input.StartsWith("#") ->
+            printfn "Invalid command."
         | input ->
             let result = Interpreter.run (tokenize input) interpreter
             match result with
