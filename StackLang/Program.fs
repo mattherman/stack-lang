9e82c330d37515962f40c04e7f420c13ee0e5fd7
Matthew Herman <mattherman11@gmail.com>
2023-02-02 09:04:44 -0600

    Allow printing vocab and defining words

--- StackLang/Program.fs
+++ StackLang/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System
 open System.IO
+open StackLang.Interpreter.Models
 open StackLang.Shared
 open StackLang.Debugger
 open StackLang.Interpreter
@@ -9,12 +10,36 @@ let printHelp () =
     printfn "Enter input to execute or a command."
     printfn "\nThe following commands are available:"
     printfn "#quit - exits the interpreter"
+    printfn "#vocab - displays all words defined in the vocabulary"
+    printfn "#define <symbol> - displays the definition of the word associated with <symbol>"
     printfn "#debug - attaches a debugger (attached by default)"
     printfn "#nodebug - detaches the debuggger"
     printfn "#step - attaches a debugger and enables step debugging"
     printfn "#nostep - disables step debugging"
     printfn "#help - displays this help information"
     printfn "\nhttps://github.com/mattherman/stack-lang"
+
+let printVocabulary (dictionary: Map<string, Word>) =
+    dictionary
+    |> Map.keys
+    |> Seq.sort
+    |> Seq.iter (fun word -> printfn $"{word}")
+
+let printDefinition (symbol: string) (dictionary: Map<string, Word>) =
+    match dictionary.TryGetValue(symbol) with
+    | true, word ->
+        printf $": {word.Symbol} "
+        match word.Instructions with
+        | Native _ ->
+            printf "<native>"
+        | Compiled instructions ->
+            let tokens =
+                instructions
+                |> List.map valueToString
+                |> String.concat " "
+            printf $"{tokens}"
+        printfn " ;"
+    | _ -> printfn $"No symbol '{symbol}' defined in the vocabulary."
 
 let runFile file =
     let lines = File.ReadAllLines(file)
@@ -38,6 +63,8 @@ let runRepl () =
         | "#quit" ->
             quit <- true
             printfn "Goodbye!"
+        | "#vocab" ->
+            printVocabulary interpreter.Dictionary
         | "#debug" ->
             attachDebugger interpreter
             printfn "Debugger attached."
@@ -50,6 +77,12 @@ let runRepl () =
         | "#nostep" ->
             disableStepDebugging interpreter
             printfn "Step debugging disabled."
+        | input when input.StartsWith("#define") ->
+            match input.Split(" ") with
+            | [| _; symbol |] ->
+                printDefinition symbol interpreter.Dictionary
+            | _ ->
+                printfn "Usage: #define <symbol>"
         | input when input.StartsWith("#") ->
             printfn "Invalid command."
         | input ->
